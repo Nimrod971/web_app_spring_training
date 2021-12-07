f824497253dc44ea68d48baa8a1d7d186cbfc71f
cypher <nimrodndoudi@gmail.com>
2021-12-07 11:30:13 +0100

    Endpoint HTTP

---
+++ src/main/java/fr/lernejo/todo/Todo.java
@@ -0,0 +1,5 @@
+package fr.lernejo.todo;
+import java.util.ArrayList;
+
+public record Todo(String message,String author) {
+}
---
+++ src/main/java/fr/lernejo/todo/TodoListController.java
@@ -0,0 +1,21 @@
+package fr.lernejo.todo;
+
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class TodoListController {
+
+    ArrayList<Todo> ListToDo = new ArrayList<>();
+
+    @GetMapping(value = "/api/todo")
+    public ArrayList<Todo> getList() {
+        return ListToDo;
+    }
+
+    @PostMapping(value = "/api/todo")
+    public void add(@RequestBody Todo todo) {
+        ListToDo.add(todo);
+    }
+}
